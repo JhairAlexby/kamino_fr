24af0cd27ef4ee6cbbdbf89f1fc4f2af7c510d84
Josep25-dev <gameplaycj2002@gmail.com>
2025-11-17 00:27:57 -0600

    rutas midoficadas

--- lib/features/1_auth/presentation/provider/register_provider.dart
+++ lib/features/1_auth/presentation/provider/register_provider.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
 import 'package:kamino_fr/features/1_auth/data/auth_repository.dart';
 import 'package:go_router/go_router.dart';
 
